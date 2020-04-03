d9af09a573b5feaec1d1acba748a0c9acdb6bd54
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2020-04-02 22:03:45 -0600

    Small test for queue project

---
+++ NewTask/NewTask.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace NewTask
+{
+    class NewTask
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "task_queue", durable: true,
+                        exclusive: false, autoDelete: false,
+                        arguments: null);
+            var msg = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(msg);
+
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
+            channel.BasicPublish(exchange: "", routingKey: "task_queue",
+                        basicProperties: properties, body: body);
+
+            Console.WriteLine($"[x] Sent {msg}");
+
+            Console.WriteLine("Pres [enter] to exit.");
+            Console.ReadLine();
+        }
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello world!");
+        }
+    }
+}
